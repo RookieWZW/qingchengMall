a94b7abef7a9fe3c6a5b45803eb9894cc8be5f3f
RookieWZW <RookieWZW@163.com>
2019-11-16 13:59:59 +0800

    init upload

---
+++ qingcheng_service_goods/src/main/java/com/qingcheng/dao/BrandMapper.java
@@ -0,0 +1,11 @@
+package com.qingcheng.dao;
+
+import com.qingcheng.pojo.goods.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author RookieWZW
+ */
+public interface BrandMapper extends Mapper<Brand> {
+
+}
